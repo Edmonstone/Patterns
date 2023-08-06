cbd262f33c3c56bf22540ed5e91f9d2bfcd010d8
Surender Venkatesan <89645185+Edmonstone@users.noreply.github.com>
2023-08-06 21:06:23 +0530

    Update Arrow.java

--- Arrow.java
+++ Arrow.java
@@ -8,13 +8,13 @@ public class Arrow {
         for (int i=1 ;i<2*n; ++i){
             if(i<=n){
                 for(int j = 1;j<i;j++){
-                    System.out.print('*');
+                    System.out.print('-');
                 }
                 System.out.println();
             }
             else{
                 for(int k=n;k>i-n;k--){
-                    System.out.print("*");
+                    System.out.print("-");
                 }
                 System.out.println("");
             }
